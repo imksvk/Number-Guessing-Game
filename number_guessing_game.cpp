38ad9d0a22c29d74ce18bf0b1a2f5a0cac417b16
imksvk <keshavkumar123456789101112@gmail.com>
2024-09-16 13:33:20 +0530

    guesser class made

---
+++ number_guessing_game.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Guesser{
+    int guessed_num;
+    public:
+        int takeguesserinput(){
+            cout<<"Give the number for the guesser"<<endl;
+            int x;
+            cin>>x;
+            guessed_num = x;
+            return guessed_num;
+        }
+};
+
+class Umpire{
+
+};
+
+class Player{
+    
+
+};
+
+int main(){
+    
+    cout<<"Welcome to Number Guessing Game"<<endl;
+    cout<<"Let's Start the Game "<<endl;
+
+
+
+
+     
+
+
+
+
+    cout<<"Let's End the Game"<<endl;
+    return 0;
+}
